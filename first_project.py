f681ba038bf0df2937bb6cee5add0053cc59ed35
alvindez <dezwasswa4@gmail.com>
2023-09-20 12:45:36 +0600

    added score

--- first_project.py
+++ first_project.py
@@ -7,26 +7,35 @@ if playing.lower() != "yes":
     quit()
 print("Okay! let's play")
 
+
+score = 0
 answer = input("What is CPU in full? ")
 if answer.lower() == "central processing unit":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect")
 
 answer = input("What is RAM in full? ")
 if answer.lower() == "random access memory":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect")
 
 answer = input("What is PU in full? ")
 if answer.lower() == "power unit":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect")
 
 answer = input("What is LCD in full? ")
 if answer.lower() == "liquid crystal display":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect")
+
+    print("You got"  +   str(score)   +  "questions correct")
+    
