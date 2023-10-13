cea0ae3de0645f707c97ba21d8ae8ff65b1f4d3a
alvindez <dezwasswa4@gmail.com>
2023-10-13 16:59:32 +0600

    bank account project

--- Bank_Account/bankaccounts.py
+++ Bank_Account/bankaccounts.py
@@ -1,3 +1,6 @@
+class BalanceException(Exception):
+    pass
+
 class BankAccount:
     def __init__(self, initialAmount, accName):
         self.balance = initialAmount
@@ -11,3 +14,20 @@ class BankAccount:
         self.balance = self.balance + amount
         print("\nDeposit Cmplete.")
         self.get_balance()
+
+    def viableTransaction(self, amount):
+        if self.balance >= amount:
+            return
+        else:
+            raise BalanceException(f"\nSorry, account {self.name} only has a balance of UGshs{self.balance}")
+    
+
+    def withdraw(self, amount):
+        try:
+            self.viableTransaction(amount)
+            self.balance = self.balance - amount
+            print("\nWithdraw Complete.")
+            self.get_balance()
+
+        except BalanceException as error:
+            print(f"\nWithdraw interrupted: {error}")
--- Bank_Account/oop_project.py
+++ Bank_Account/oop_project.py
@@ -7,3 +7,6 @@ Alvin.get_balance()
 Fify.get_balance()
 
 Alvin.deposit(500000)
+
+Fify.withdraw(2100000)
+Fify.withdraw(200000)
