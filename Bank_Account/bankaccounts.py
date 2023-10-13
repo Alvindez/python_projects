e99ea740668638988b172b90906ebe3af68ffb73
alvindez <dezwasswa4@gmail.com>
2023-10-13 16:13:52 +0600

    oop tryouts

--- Bank_Account/bankaccounts.py
+++ Bank_Account/bankaccounts.py
@@ -3,3 +3,11 @@ class BankAccount:
         self.balance = initialAmount
         self.name = accName
         print(f"\nAccount {self.name} created. \nBalance = UGshs{self.balance}")
+
+    def get_balance(self):
+        print(f"\nAccount {self.name} \nbalance = UGshs{self.balance}")
+
+    def deposit(self, amount):
+        self.balance = self.balance + amount
+        print("\nDeposit Cmplete.")
+        self.get_balance()
--- Bank_Account/oop_project.py
+++ Bank_Account/oop_project.py
@@ -2,3 +2,8 @@ from bankaccounts import *
 
 Alvin = BankAccount(1600000, "Alvin")
 Fify = BankAccount(2000000, "Fify")
+
+Alvin.get_balance()
+Fify.get_balance()
+
+Alvin.deposit(500000)
