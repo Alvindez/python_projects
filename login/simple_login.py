4ddd067238142fa269d3a6ad9c1c54ec390812c4
alvindez <dezwasswa4@gmail.com>
2023-10-17 19:37:24 +0600

    deductions

--- login/simple_login.py
+++ login/simple_login.py
@@ -3,10 +3,6 @@
 try:
     users = [['alvin', 'a123'],['ben', 'gh_3tw'],['Chris', '@quc98']]
 
-    # def add_users():
-    #     users.append('cliff', 'cl468f')
-    #     print (users)
-
     def login(users):
         while True:
             username = input("Please enter a username: ")
